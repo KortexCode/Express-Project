22a4a3da01acfec97c1d44750900e973041653e7
kortexcode <darkmaeno323@gmail.com>
2023-06-13 12:06:28 -0500

    Se realiza ejercicio de enrutamiento, se crea un servidor http, se usa el método Get, se eliminan dependencias de typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,11 @@ module.exports = {
 		commonjs: true,
 		es2021: true,
 	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'eslint-config-prettier',
-		'prettier',
-	],
+	extends: ['eslint:recommended', 'eslint-config-prettier', 'prettier'],
 	overrides: [],
-	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 	},
-	plugins: ['@typescript-eslint'],
 	rules: {
 		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'unix'],
